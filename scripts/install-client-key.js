78d694e5edde29d673967d6717920d1bad4f5d08
ichizokichinosuke <nuuuukemw@gmail.com>
2022-06-17 16:09:54 +0900

    remove script tag in js file

--- scripts/install-client-key.js
+++ scripts/install-client-key.js
@@ -1,14 +1,12 @@
-<script>
-  !function (a, b, c, d, e, f){
-    a.ddjskey = e;
-    a.ddoptions = f || null;
-    var m = b.createElement(c), n = b.getElementsByTagName(c)[0];
-    m.async = 1, m.src = d, n.parentNode.insertBefore(m, n);
-  }(window,
-    document,
-    "script",
-    "https://js.datadome.co/tags.js",
-    "328766FF935116A3A9277B028DE37D",
-    { ajaxListenerPath: true }
-  );
-</script>
+!function (a, b, c, d, e, f){
+a.ddjskey = e;
+a.ddoptions = f || null;
+var m = b.createElement(c), n = b.getElementsByTagName(c)[0];
+m.async = 1, m.src = d, n.parentNode.insertBefore(m, n);
+}(window,
+document,
+"script",
+"https://js.datadome.co/tags.js",
+"328766FF935116A3A9277B028DE37D",
+{ ajaxListenerPath: true }
+);
